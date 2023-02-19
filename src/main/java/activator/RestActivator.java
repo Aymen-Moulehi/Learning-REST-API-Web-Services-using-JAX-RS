750ad22b3da318f4634c921050a34d6a7fd468ea
aymen <aymen.moulehi@outlook.com>
2023-02-19 14:08:58 +0100

    Included Ex 1 result screenshots.

--- src/main/java/activator/RestActivator.java
+++ src/main/java/activator/RestActivator.java
@@ -1,2 +1,8 @@
-package activator;public class RestActivator {
+package activator;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestActivator extends Application {
 }
